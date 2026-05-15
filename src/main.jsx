29915435d110e8b0e02fdb8147da9f092f16deab
Anthony-Baracho <anthonybaracho0@gmail.com>
2026-05-15 00:08:18 -0300

    feat: adicionando react route e criando paginas para navegar

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,36 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import Home from './pages/Home.jsx';
+import Releases from './pages/Releases.jsx';
+import Kpis from './pages/Kpis.jsx';
+import Investments from './pages/Investments.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+
+  {
+    path: "releases",
+    element: <Releases />
+  },
+
+  {
+    path: "kpis",
+    element: <Kpis />
+  },
+
+  {
+    path: "investments",
+    element: <Investments />
+  }]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ src/pages/Releases.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const releases = () => {
+  return (
+    <div>
+      <h1>Lançamentos de Despesas</h1>
+    </div>
+  )
+}
+
+export default releases
