711c78c4ed36010fe7f890cd018de826b00b2f5d
Kelvin <152228723+R-Kelvin@users.noreply.github.com>
2026-05-16 11:47:39 -0300

    login 0.1

---
+++ server.js
@@ -0,0 +1,30 @@
+import express, { request } from 'express'
+
+const app = express()
+app.use(express.json())
+
+const users = []
+
+app.post('/users', (req, res) => {
+    
+    users.push(req.body)
+
+    res.status(201).json(req.body)
+})
+
+app.get('/users', (req, res) => {
+    res.status(200).json(users)
+})
+
+app.listen(3000)
+
+/*
+MongoDB
+
+usuario: ruank256_db_user
+senha: oE7zgb9aBkLivDRH
+
+iniciar servidor: node --watch server.js
+parar servidor: ctrl + C
+iniciar o prisma: npx prisma init
+*/
